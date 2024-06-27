e2624155a35a926092c92540a68c991fa37e8d83
hadiheidariazar <hadiha010101@gmail.com>
2024-06-27 20:36:31 +0330

    add the Shop.jsx file and its router

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,9 @@
 import Home from "./Pages/Home/Home";
+import Shop from "./Pages/Shop/Shop";
 
 const routes = [
-    {path: "/", element: <Home />}
+    {path: "/", element: <Home />},
+    {path: "/shop", element: <Shop />},
 ]
 
 export default routes
