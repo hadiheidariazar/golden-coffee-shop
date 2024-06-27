fb1a939951d7a5b40303ceb844fbfafa7a1ed83b
hadiheidariazar <hadiha010101@gmail.com>
2024-06-27 23:23:39 +0330

    add the Register.jsx file and its router

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import Blogs from "./Pages/Blogs/Blogs";
 import ContactUs from "./Pages/ContactUs/ContactUs";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 import Shop from "./Pages/Shop/Shop";
 
 const routes = [
@@ -12,6 +13,7 @@ const routes = [
     {path: "/about-us", element: <AboutUs />},
     {path: "/contact-us", element: <ContactUs />},
     {path: "/login", element: <Login />},
+    {path: "/register", element: <Register />},
 ]
 
 export default routes
