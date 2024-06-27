38351c85897ec59dd7b4de6f0c8bde3f9bf9de45
hadiheidariazar <hadiha010101@gmail.com>
2024-06-27 21:07:04 +0330

    add the AboutUs.jsx file and its router

---
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../../Components/Footer/Footer'
+import Header from '../../Components/Header/Header'
+import TitlePage from '../../Components/TitlePage/TitlePage'
+
+export default function AboutUs() {
+  return (
+    <>
+        <TitlePage title="درباره ما" />
+        <Header />
+        <Footer />
+    </>
+  )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,4 @@
+import AboutUs from "./Pages/AboutUs/AboutUs";
 import Blogs from "./Pages/Blogs/Blogs";
 import Home from "./Pages/Home/Home";
 import Shop from "./Pages/Shop/Shop";
@@ -6,6 +7,7 @@ const routes = [
     {path: "/", element: <Home />},
     {path: "/shop", element: <Shop />},
     {path: "/blogs", element: <Blogs />},
+    {path: "/about-us", element: <AboutUs />},
 ]
 
 export default routes
