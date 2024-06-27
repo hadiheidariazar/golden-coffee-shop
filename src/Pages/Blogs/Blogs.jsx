3d4326681600dcabd4c573b6360332aea6989ca3
hadiheidariazar <hadiha010101@gmail.com>
2024-06-27 20:59:34 +0330

    coding blog header section in Blogs.jsx file

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -4,11 +4,14 @@ import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
 
 export default function Blogs() {
-  return (
-    <>
-        <TitlePage title="بلاگ ها" />
-        <Header />
-        <Footer />
-    </>
-  )
+    return (
+        <>
+            <TitlePage title="بلاگ ها" />
+            <Header />
+            <section className='blog-header mt-16 md:mt-0 h-60 xs:h-80 md:h-screen bg-blog-header bg-no-repeat bg-cover bg-[right_top]'>
+                <div className="container flex items-center h-full"></div>
+            </section>
+            <Footer />
+        </>
+    )
 }
