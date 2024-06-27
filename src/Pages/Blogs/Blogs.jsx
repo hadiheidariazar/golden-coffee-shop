cd95f3d7ca947d58964464ee4fa68a584c73377a
hadiheidariazar <hadiha010101@gmail.com>
2024-06-27 20:57:54 +0330

    add the Blogs.jsx file and its router

---
+++ src/Pages/Blogs/Blogs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TitlePage from '../../Components/TitlePage/TitlePage'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
+
+export default function Blogs() {
+  return (
+    <>
+        <TitlePage title="بلاگ ها" />
+        <Header />
+        <Footer />
+    </>
+  )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,11 @@
+import Blogs from "./Pages/Blogs/Blogs";
 import Home from "./Pages/Home/Home";
 import Shop from "./Pages/Shop/Shop";
 
 const routes = [
     {path: "/", element: <Home />},
     {path: "/shop", element: <Shop />},
+    {path: "/blogs", element: <Blogs />},
 ]
 
 export default routes
