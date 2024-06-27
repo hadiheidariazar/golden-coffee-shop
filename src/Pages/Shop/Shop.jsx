cc039441149c2b20a265a42a11dc88f2f1afed71
hadiheidariazar <hadiha010101@gmail.com>
2024-06-27 20:42:29 +0330

    coding shop header section in Shop.jsx file

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import TitlePage from '../../Components/TitlePage/TitlePage'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
 
 export default function Shop() {
   return (
-    <div>Shop</div>
+    <>
+        <TitlePage title="فروشگاه" />
+        <Header />
+        <section className='shop-header mt-7 md:mt-0 h-60 xs:h-80 md:h-screen bg-shop-header bg-no-repeat bg-cover bg-[center_top] xs:bg-[left_top]'>
+                <div className="container flex items-center h-full">
+                    <h1 className='text-2xl xs:text-3xl sm:text-4xl md:text-5xl ipad:text-6xl select-none font-morabba-bold text-white'> فروشگاه محصولات </h1>
+                </div>
+            </section>
+        <Footer />
+    </>
   )
 }
