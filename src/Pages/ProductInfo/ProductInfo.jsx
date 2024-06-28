97176cfd52ca1c10008de1d02ebbd9b787d71c1c
hadiheidariazar <hadiha010101@gmail.com>
2024-06-28 23:45:01 +0330

    coding breadcrumb section in header ProductInfo.jsx file

--- src/Pages/ProductInfo/ProductInfo.jsx
+++ src/Pages/ProductInfo/ProductInfo.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import TitlePage from '../../Components/TitlePage/TitlePage'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
+import Breadcrumb from '../../Components/Breadcrumb/Breadcrumb'
 
 export default function ProductInfo() {
-  return (
-    <>
-        <TitlePage title="اطلاعات محصول" />
-        <Header />
-        <Footer />
-    </>
-  )
+    return (
+        <>
+            <TitlePage title="اطلاعات محصول" />
+            <Header />
+            <section className='product-info mt-28 md:mt-40 mb-16 md:mb-28'>
+                <div className="container">
+                    <Breadcrumb
+                        firstTitle="لوازم جانبی و تجهیزات"
+                        firsTitletLink="/category/coffee"
+                        lastTitle="قهوه اسپرسو بن مانو مدل پریسکا 250 گرمی"
+                        isBg={true}
+                    />
+                </div>
+            </section>
+            <Footer />
+        </>
+    )
 }
