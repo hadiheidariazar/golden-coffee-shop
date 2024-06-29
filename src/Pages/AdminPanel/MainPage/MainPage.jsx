c22bd52ba72d4729a84a2d5fc01a8f1a5cbac880
hadiheidariazar <hadiha010101@gmail.com>
2024-06-29 13:47:33 +0330

    coding header counter boxes at MainPage.jsx file in Admin Panel page

--- src/Pages/AdminPanel/MainPage/MainPage.jsx
+++ src/Pages/AdminPanel/MainPage/MainPage.jsx
@@ -1,7 +1,50 @@
 import React from 'react'
+import TitlePage from '../../../Components/TitlePage/TitlePage'
+import CounterBox from '../../../Components/CounterBox/CounterBox'
 
 export default function MainPage() {
-  return (
-    <div>MainPage</div>
-  )
+    return (
+        <>
+            <TitlePage title="صفحه اصلی پنل مدیریت" />
+            <section>
+                <div className='topbar'>
+                    <nav className='grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 justify-center items-center gap-3'>
+                        <CounterBox
+                            boxColor="bg-sky-500"
+                            title="ثبت نامی ها"
+                            count={1500}
+                            subTitle="کاربر"
+                            iconBg="bg-sky-300/50"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className="w-7 text-white">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                            </svg>
+                        </CounterBox>
+                        <CounterBox
+                            boxColor="bg-rose-500"
+                            title="میزان فروش"
+                            count={80}
+                            subTitle="درصد"
+                            iconBg="bg-rose-300/50"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className="w-7 text-white">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
+                            </svg>
+                        </CounterBox>
+                        <CounterBox
+                            boxColor="bg-emerald-500"
+                            title="میزان سود"
+                            count={72}
+                            subTitle="درصد"
+                            iconBg="bg-emerald-300/50"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className="w-7 text-white">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5m.75-9 3-3 2.148 2.148A12.061 12.061 0 0 1 16.5 7.605" />
+                            </svg>
+                        </CounterBox>
+                    </nav>
+                </div>
+            </section>
+        </>
+    )
 }
