3b077b5f2a3eb6c05c7f123b54fb56220d2ddf82
hadiheidariazar <hadiha010101@gmail.com>
2024-06-29 13:35:23 +0330

    coding sidebar for Admin Panel page in Index.jsx file

---
+++ src/Components/AdminPanelSidebar/AdminPanelSidebar.jsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useRef } from 'react'
+import { NavLink } from 'react-router-dom'
+import Swal from 'sweetalert2'
+
+export default function AdminPanelSidebar({ setShow }) {
+
+    const homePage = useRef()
+
+    const isActiveMenu = (event) => {
+        if (setShow) {
+            setShow(false)
+        }
+
+        if (event.target.textContent === 'صفحه اصلی') {
+            homePage.current.classList.add('bg-orange-400')
+            homePage.current.classList.add('text-white')
+        } else {
+            homePage.current.classList.remove('bg-orange-400')
+            homePage.current.classList.remove('text-white')
+        }
+    }
+
+    useEffect(() => {
+        if (location.pathname === '/admin-panel' || location.pathname === '/admin-panel/') {
+            homePage.current.classList.add('bg-orange-400')
+            homePage.current.classList.add('text-white')
+        } else {
+            homePage.current.classList.remove('bg-orange-400')
+            homePage.current.classList.remove('text-white')
+        }
+    }, [])
+
+    const showAlert = () => {
+        Swal.fire({
+            text: 'شما با موفقیت از حساب کاربری خود خارج شدید',
+            icon: 'success',
+            confirmButtonText: 'متوجه شدم!'
+        })
+    }
+
+    return (
+        <ul className="space-y-2 ipad:space-y-3">
+            <li onClick={(event) => isActiveMenu(event)}>
+                <NavLink to='/admin-panel' className={(link) => (link.isActive ? "flex items-center bg-orange-400 hover:bg-orange-400 hover:text-white text-white p-2 rounded text-base transition-colors" : "flex items-center p-2 rounded hover:bg-orange-400 hover:text-white transition-colors text-base")} ref={homePage}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 ml-1">
+                        <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                        <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+                    </svg>
+                    صفحه اصلی
+                </NavLink>
+            </li>
+            <li onClick={(event) => isActiveMenu(event)}>
+                <NavLink to='products' className={(link) => (link.isActive ? "flex items-center bg-orange-400 text-white p-2 rounded text-base" : "flex items-center p-2 rounded hover:bg-orange-400 hover:text-white transition-colors text-base")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 ml-1">
+                        <path fillRule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z" clipRule="evenodd" />
+                    </svg>
+
+                    کالاها
+                </NavLink>
+            </li>
+            <li onClick={(event) => isActiveMenu(event)}>
+                <NavLink to='blogs' className={(link) => (link.isActive ? "flex items-center bg-orange-400 text-white p-2 rounded text-base" : "flex items-center p-2 rounded hover:bg-orange-400 hover:text-white transition-colors text-base")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 ml-1">
+                        <path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z" />
+                    </svg>
+                    بلاگ ها
+                </NavLink>
+            </li>
+            <li onClick={(event) => isActiveMenu(event)}>
+                <NavLink to='users' className={(link) => (link.isActive ? "flex items-center bg-orange-400 text-white p-2 rounded text-base" : "flex items-center p-2 rounded hover:bg-orange-400 hover:text-white transition-colors text-base")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 ml-1">
+                        <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
+                    </svg>
+
+                    کاربران
+                </NavLink>
+            </li>
+            <li onClick={(event) => isActiveMenu(event)}>
+                <NavLink to='comments' className={(link) => (link.isActive ? "flex items-center bg-orange-400 text-white p-2 rounded text-base" : "flex items-center p-2 rounded hover:bg-orange-400 hover:text-white transition-colors text-base")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 ml-1">
+                        <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 0 0-1.032-.211 50.89 50.89 0 0 0-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 0 0 2.433 3.984L7.28 21.53A.75.75 0 0 1 6 21v-4.03a48.527 48.527 0 0 1-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979Z" />
+                        <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 0 0 1.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0 0 15.75 7.5Z" />
+                    </svg>
+
+                    دیدگاه ها
+                </NavLink>
+            </li>
+            <li onClick={(event) => isActiveMenu(event)}>
+                <NavLink to='messages' className={(link) => (link.isActive ? "flex items-center bg-orange-400 text-white p-2 rounded text-base" : "flex items-center p-2 rounded hover:bg-orange-400 hover:text-white transition-colors text-base")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 ml-1">
+                        <path fillRule="evenodd" d="M12 2.25c-2.429 0-4.817.178-7.152.521C2.87 3.061 1.5 4.795 1.5 6.741v6.018c0 1.946 1.37 3.68 3.348 3.97.877.129 1.761.234 2.652.316V21a.75.75 0 0 0 1.28.53l4.184-4.183a.39.39 0 0 1 .266-.112c2.006-.05 3.982-.22 5.922-.506 1.978-.29 3.348-2.023 3.348-3.97V6.741c0-1.947-1.37-3.68-3.348-3.97A49.145 49.145 0 0 0 12 2.25ZM8.25 8.625a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Zm2.625 1.125a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z" clipRule="evenodd" />
+                    </svg>
+
+                    پیام ها
+                </NavLink>
+            </li>
+            <li onClick={(event) => isActiveMenu(event)}>
+                <NavLink to='tickets' className={(link) => (link.isActive ? "flex items-center bg-orange-400 text-white p-2 rounded text-base" : "flex items-center p-2 rounded hover:bg-orange-400 hover:text-white transition-colors text-base")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 ml-1">
+                        <path fillRule="evenodd" d="M1.5 6.375c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v3.026a.75.75 0 0 1-.375.65 2.249 2.249 0 0 0 0 3.898.75.75 0 0 1 .375.65v3.026c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 17.625v-3.026a.75.75 0 0 1 .374-.65 2.249 2.249 0 0 0 0-3.898.75.75 0 0 1-.374-.65V6.375Zm15-1.125a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0V6a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0v.75a.75.75 0 0 0 1.5 0v-.75Zm-.75 3a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0v-.75a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0V18a.75.75 0 0 0 1.5 0v-.75ZM6 12a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H6.75A.75.75 0 0 1 6 12Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clipRule="evenodd" />
+                    </svg>
+
+                    تیکت ها
+                </NavLink>
+            </li>
+            <li onClick={(event) => isActiveMenu(event)}>
+                <NavLink to='categories' className={(link) => (link.isActive ? "flex items-center bg-orange-400 text-white p-2 rounded text-base" : "flex items-center p-2 rounded hover:bg-orange-400 hover:text-white transition-colors text-base")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 ml-1">
+                        <path fillRule="evenodd" d="M2.25 13.5a8.25 8.25 0 0 1 8.25-8.25.75.75 0 0 1 .75.75v6.75H18a.75.75 0 0 1 .75.75 8.25 8.25 0 0 1-16.5 0Z" clipRule="evenodd" />
+                        <path fillRule="evenodd" d="M12.75 3a.75.75 0 0 1 .75-.75 8.25 8.25 0 0 1 8.25 8.25.75.75 0 0 1-.75.75h-7.5a.75.75 0 0 1-.75-.75V3Z" clipRule="evenodd" />
+                    </svg>
+
+                    دسته بندی ها
+                </NavLink>
+            </li>
+            <li>
+                <button className="flex items-center p-2 w-full rounded hover:bg-orange-400 hover:text-white transition-colors text-base" onClick={showAlert}>
+                    <svg stroke="currentColor" fill="currentColor" strokeWidth="0" version="1.1" viewBox="0 0 16 16" className="w-4 h-4 ml-1.5" xmlns="http://www.w3.org/2000/svg"><path d="M12 10v-2h-5v-2h5v-2l3 3zM11 9v4h-5v3l-6-3v-13h11v5h-1v-4h-8l4 2v9h4v-3z"></path></svg>
+
+                    خروج
+                </button>
+            </li>
+        </ul>
+    )
+}
