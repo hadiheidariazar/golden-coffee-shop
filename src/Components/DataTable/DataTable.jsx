f858c90859931a605e32e886d809beb09691cbbe
hadiheidariazar <hadiha010101@gmail.com>
2024-06-29 13:52:24 +0330

    coding Admin Panel Data Table component

---
+++ src/Components/DataTable/DataTable.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function DataTable({ headerItemCount, headerItemTitle, breakPoint, setionsTableWidth, children }) {
+    return (
+        <table className={`pe-3 grid overflow-x-auto ${breakPoint}`}>
+            <thead>
+                <tr className='flex justify-between items-center px-2 py-1 w-full child:font-morabba-medium child:text-sm sm:child:text-base child:text-center h-max bg-orange-200/70 dark:bg-zinc-600 text-orange-600 dark:text-orange-400 rounded'>
+                    <th className='w-11'> شناسه </th>
+                    {
+                        new Array(headerItemCount).fill(0).map((item, index) => (
+                            <th key={index + 1} className={setionsTableWidth[index]}>{headerItemTitle[index]}</th>
+                        ))
+                    }
+                </tr>
+            </thead>
+            <tbody className='w-full grid child:flex child:justify-between child:items-start child:gap-x-2 child:w-full child:text-xs child:font-dana-medium sm:child:text-sm sm:child:font-dana child:text-center px-1 child:py-2'>
+                {children}
+            </tbody>
+        </table>
+    )
+}
