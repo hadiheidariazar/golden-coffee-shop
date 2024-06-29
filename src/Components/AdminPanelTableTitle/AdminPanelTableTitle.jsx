7d47e5f456b0f7cbb27086c9b71c22bdb1b5b474
hadiheidariazar <hadiha010101@gmail.com>
2024-06-29 13:51:08 +0330

    coding Admin Panel Table Title component

---
+++ src/Components/AdminPanelTableTitle/AdminPanelTableTitle.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function AdminPanelTableTitle({ title, isList, breakPoint, children }) {
+    return (
+        <div className='w-full bg-gray-100 dark:bg-zinc-800 py-5 rounded shadow-lg'>
+            <div className='mx-6 font-morabba-medium'>
+                <span>{isList ? "لیست" : "افزودن"}</span>
+                <span className='text-orange-600 dark:text-orange-400'> {title} </span>
+            </div>
+            <div className='mt-4 xs:mt-5 px-3 xs:px-5 w-full overflow-auto'>
+                <div className={breakPoint}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
